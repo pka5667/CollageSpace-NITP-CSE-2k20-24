28c26e2c92a239c70d30d7c8017f4ec7b0a47c16
pka5667 <63954083+pka5667@users.noreply.github.com>
2021-01-11 14:41:43 +0530

    Update Notes.js

--- js/Notes.js
+++ js/Notes.js
@@ -1,10 +1,10 @@
 var Notes = [
-    ["Maths Notes 11 Jan 2021","11-1-2021","https://drive.google.com/file/d/1sim33cw6TiDou-jOMxY3vtdJF8L9swDY/view?usp=sharing"],
-    ["Maths Notes 8 Jan 2021","8-1-2021","https://drive.google.com/file/d/1sunIwqVK7plpe9rLIJcecp3gs4Lbr9re/view?usp=sharing"],
-    ["Maths Notes 6 Jan 2021","6-1-2021","https://drive.google.com/file/d/15nM-fXbdQbDrD3JHYCytwhY7UooeDAlg/view?usp=sharing"],
-    ["Maths Notes 4 Jan 2021","4-1-2021","https://drive.google.com/file/d/1qakqfvBGAQycQJK-xFF696o-pX-SkZCZ/view?usp=sharing"],
-    ["Maths lactures notes 1 Dec 2020 to 3 Jan 2021","3-1-2021","https://drive.google.com/file/d/1YX3nVK2Jz-gLIRLF0KnMziwXtMzAEqm0/view?usp=sharing"],
-    ["Maths lacture1 Successive Diffrentiation","11-12-2020","https://drive.google.com/file/d/1wSYjfkQEXyG4E2UUVwZ9FJdYGde_V4sD/view?usp=sharing"],
+    ["Maths Notes 11 Jan 2021 ~By Deepanshu Tibrewal","11-1-2021","https://drive.google.com/file/d/1sim33cw6TiDou-jOMxY3vtdJF8L9swDY/view?usp=sharing"],
+    ["Maths Notes 8 Jan 2021 ~By Deepanshu Tibrewal","8-1-2021","https://drive.google.com/file/d/1sunIwqVK7plpe9rLIJcecp3gs4Lbr9re/view?usp=sharing"],
+    ["Maths Notes 6 Jan 2021 ~By Deepanshu Tibrewal","6-1-2021","https://drive.google.com/file/d/15nM-fXbdQbDrD3JHYCytwhY7UooeDAlg/view?usp=sharing"],
+    ["Maths Notes 4 Jan 2021 ~By Deepanshu Tibrewal","4-1-2021","https://drive.google.com/file/d/1qakqfvBGAQycQJK-xFF696o-pX-SkZCZ/view?usp=sharing"],
+    ["Maths lactures notes 1 Dec 2020 to 3 Jan 2021 ~By Deepanshu Tibrewal","3-1-2021","https://drive.google.com/file/d/1YX3nVK2Jz-gLIRLF0KnMziwXtMzAEqm0/view?usp=sharing"],
+    ["Maths lacture1 Successive Diffrentiation ~By Deepanshu Tibrewal","11-12-2020","https://drive.google.com/file/d/1wSYjfkQEXyG4E2UUVwZ9FJdYGde_V4sD/view?usp=sharing"],
     ["CS1401 L2 Notes ~By ⁨Shri Vaishnavi Vinnakota","10-12-2020","https://drive.google.com/file/d/1e7DnWJZM0KyhWfQ4IKHbfMb4yVFShxrQ/view?usp=sharing"],
     ["CS1401 L1 Notes ~By ⁨Shri Vaishnavi Vinnakota", "9-12-2020", "https://drive.google.com/file/d/1WwsmUhbp2BftvNkOhH0443Tt8mMJ3jqP/view?usp=sharing"]
 ]
