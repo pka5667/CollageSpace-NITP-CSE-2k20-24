089d8bd440055100ecc9022e3c330fa24ebf62d3
pka5667 <63954083+pka5667@users.noreply.github.com>
2021-01-11 13:37:20 +0530

    Update Notes.js

--- js/Notes.js
+++ js/Notes.js
@@ -1,5 +1,6 @@
 var Notes = [
-    ["Maths lactures notes 1 Dec 2020 to 3 Jan 2021","11-1-2021","https://drive.google.com/file/d/1YX3nVK2Jz-gLIRLF0KnMziwXtMzAEqm0/view?usp=sharing"],
+    ["Maths Notes 4 Jan 2021","4-1-2021","https://drive.google.com/file/d/1qakqfvBGAQycQJK-xFF696o-pX-SkZCZ/view?usp=sharing"],
+    ["Maths lactures notes 1 Dec 2020 to 3 Jan 2021","3-1-2021","https://drive.google.com/file/d/1YX3nVK2Jz-gLIRLF0KnMziwXtMzAEqm0/view?usp=sharing"],
     ["Maths lacture1 Successive Diffrentiation","11-12-2020","https://drive.google.com/file/d/1wSYjfkQEXyG4E2UUVwZ9FJdYGde_V4sD/view?usp=sharing"],
     ["CS1401 L2 Notes ~By ⁨Shri Vaishnavi Vinnakota","10-12-2020","https://drive.google.com/file/d/1e7DnWJZM0KyhWfQ4IKHbfMb4yVFShxrQ/view?usp=sharing"],
     ["CS1401 L1 Notes ~By ⁨Shri Vaishnavi Vinnakota", "9-12-2020", "https://drive.google.com/file/d/1WwsmUhbp2BftvNkOhH0443Tt8mMJ3jqP/view?usp=sharing"]
