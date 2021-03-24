6521f71a4d98b668b92d089dd96e71439bda38e3
pka5667 <63954083+pka5667@users.noreply.github.com>
2021-03-24 18:20:56 +0530

    Update Notice.js

--- js/Notice.js
+++ js/Notice.js
@@ -1,6 +1,6 @@
 var Notice = [
-    ["Time Table CSE1 (UnOfficial)", "25-12-2020", "https://patnanit-my.sharepoint.com/:w:/g/personal/pulkita_ug20_cse_nitp_ac_in/EdpRyJ3T3hFPgjX3kT8-VRYBDLk7CCvpPbgJn3FB3SxU1w?e=CmqwNa"],
-    ["Time Table CSE2 (UnOfficial)", "25-12-2020", "https://drive.google.com/file/d/1zIiZfzvPP9DoSqaM_jyP9GzdJOAL8kHY/view?usp=sharing"],
+    ["Time Table CSE1", "25-12-2020", "https://patnanit-my.sharepoint.com/:w:/g/personal/pulkita_ug20_cse_nitp_ac_in/EdpRyJ3T3hFPgjX3kT8-VRYBDLk7CCvpPbgJn3FB3SxU1w?e=CmqwNa"],
+    ["Time Table CSE2", "25-12-2020", "https://drive.google.com/file/d/1zIiZfzvPP9DoSqaM_jyP9GzdJOAL8kHY/view?usp=sharing"],
     ["Proteus for windows","Password is www.up4pc.com","https://drive.google.com/file/d/1a2p1_Hen014fTAMk4YmMxGQOxR274R9H/view?usp=sharing"],
     ["Academic calender for 1st year 2020","1-12-2020","https://drive.google.com/file/d/1qnXM0TXnrXjGQKZdc3kSxhBgKYkBjbpI/view?usp=sharing"],
     ["Time Table (Official)", "10-12-2020", "http://www.nitp.ac.in/uploads20/CSED_1stYear_Dec-March-2020-semester_08_12_2020.pdf"],
